f3fb759f8568b6f91e429a0e7e87baed1d9d0b70
pbmax <pkeilbarnes@gmail.com>
2016-12-25 12:14:45 -0500

    Add files via upload

--- fill-in-the-blanks-PB.py
+++ fill-in-the-blanks-PB.py
@@ -1,5 +1,5 @@
 # STAGE 2 FINAL PROJECT: Fill-in-the-Blanks Game
-# Parker (Dec 24, 2016)
+# Dec 25, 2016
 
 # Global inputs
 levels = ["easy","medium","hard"]
@@ -7,6 +7,7 @@ intro = "\nPlease select a game difficulty by typing it in.\nPossible choices: e
 paragraph_intro = "\nThe current paragraph reads as such:\n"
 q_prompt = "\nEnter the correct word for blank "
 space_holders = ["__1__","__2__","__3__","__4__"]
+allowed_error_count = 3
 
 # EASY level inputs
 answers_easy = ["pwd","ls","cd","mkdir"]
@@ -21,24 +22,24 @@ answers_hard = ["find","replace","while","or"]
 paragraph_hard = "Review of common Python methods! The __1__ method returns the starting index of the substring. The __2__ method replaces all instances of the old string with the new string. __3__ loops repeat an action until the expression is True. The __4__ operator evaluates whether the 1st expression is True, and does not evaluate the second expression if it is."
 
 def word_answer(word,answers,index):
-    # This function takes as inputs a word, an answer set, and an index
-    # and outputs either the word (if it equals the answer) or the space_holder (if the word is not equal to the answer)
+"""This function takes as inputs a word, an answer set, and an index
+and outputs either the word (if it equals the answer) or the space_holder (if the word is not equal to the answer)"""
     if word == answers[index-1]:
         return word
     else:
         return space_holders[index-1]
 
 def word_in_spaceholder(word, space_holder):
-    # This function takes as inputs a word and a specific "spaceholder" (e.g. "__1__" )
-    # and returns as output either the word itself (if equal to the spaceholder) or nothing (if the word is not equal to the spaceholder)
+"""This function takes as inputs a word and a specific "spaceholder" (e.g. "__1__" )
+and returns as output either the word itself (if equal to the spaceholder) or nothing (if the word is not equal to the spaceholder)"""
     if word == space_holder:
             return word
     else:
         return None
 
 def replace_words(paragraph,space_holder,user_answer):
-    # This function takes in a paragraph, a space_holder and a user's answer,
-    # and replaces all instances of the spaceholder with the user's answer (when correct).
+"""This function takes in a paragraph, a space_holder and a user's answer,
+and replaces all instances of the spaceholder with the user's answer (when correct)."""
     replaced = []
     paragraph = paragraph.split()
     for word in paragraph:
@@ -52,8 +53,8 @@ def replace_words(paragraph,space_holder,user_answer):
     return paragraph
 
 def fill_in_the_blanks(paragraph,space_holders,answers):
-    # This function takes as inputs a paragraph, a set of placeholders, and an answer set.
-    # It integrates the above functions, and calls for user input based on the paragraph prompts
+"""This function takes as inputs a paragraph, a set of placeholders, and an answer set.
+It integrates the above functions, and calls for user input based on the paragraph prompts"""
     print paragraph_intro
     print paragraph
     index = 0
@@ -69,15 +70,15 @@ def fill_in_the_blanks(paragraph,space_holders,answers):
                 paragraph = replace_words(paragraph,space_holders[index-1],user_answer)
                 print paragraph_intro
                 print paragraph
-            elif count == 3:
+            elif count == allowed_error_count:
                 print "\nSorry, you've guessed incorrectly too many times in a row. Try again soon!"
                 exit()
             else:
-                print "\nNot quite right...try again!\n\nYou have " + str(3-count) + " chances left..."
+                print "\nNot quite right...try again!\n\nYou have " + str(allowed_error_count-count) + " chances left..."
             count += 1
 
 def play_the_game():
-    # This integrates all of the functions and inputs above into a game with 3 possible levels (easy, medium, and hard).
+"""This integrates all of the functions and inputs above into a game with 3 possible levels (easy, medium, and hard)."""
     print intro
     user_level = raw_input("Type in a level: ")
     print "You chose: " + user_level + ".\n"
